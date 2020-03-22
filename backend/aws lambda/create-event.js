bc2b7f670f79b0751cc8987796faa03d715dfb31
Mathias Lübbe <ml@dasburo.com>
2020-03-22 08:03:55 +0100

    aws lambda functions

---
+++ backend/aws lambda/create-event.js
@@ -0,0 +1,43 @@
+const AWS = require("aws-sdk");
+const crypto = require("crypto");
+
+// Generate unique id with no external dependencies
+const generateUUID = () => crypto.randomBytes(16).toString("hex");
+
+// Generate Timestamp 
+const buildTimestamp = () => new Date().toISOString().
+  replace(/T/, ' ').      // replace T with a space
+  replace(/\..+/, '')     // delete the dot and everything after
+  
+// Initialising the DynamoDB SDK
+const documentClient = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async event => {
+  const { zipCode, temperature, dryCaught, sniff, bodyache, headache } = JSON.parse(event.body);
+  const params = {
+    TableName: "events", // The name of your DynamoDB table
+    Item: { // Creating an Item with a unique id and with the passed title
+      id: generateUUID(),
+      timestamp: buildTimestamp(),
+      zipCode: zipCode,
+        temperature : temperature,
+        dryCaught: dryCaught,
+        sniff: sniff,
+        bodyache: bodyache,
+        headache: headache
+    }
+  };
+  try {
+    // Utilising the put method to insert an item into the table (https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/GettingStarted.NodeJs.03.html#GettingStarted.NodeJs.03.01)
+    const data = await documentClient.put(params).promise();
+    const response = {
+      statusCode: 200
+    };
+    return response; // Returning a 200 if the item has been inserted 
+  } catch (e) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(e)
+    };
+  }
+};
---
+++ backend/aws lambda/create-event.test.js
@@ -0,0 +1,3 @@
+{
+    "body": "{\"zipCode\": \"10117\",\"temperature\": 38.7,\"dryCaught\": true,\"sniff\": false,\"bodyache\": true,\"headache\": false}"
+  }
